6acda4190bcf0b4a59384a6fd41ab3de14bc8aae
six-shot <okhuomonajayi54@gmail.com>
2024-04-16 16:35:19 +0000

    overview

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,4 +1,9 @@
-import add from "@/public/folder-add.png"
+import add from "@/public/folder-add.png";
+import calendar from "@/public/calendar.png";
+import calendarshadow from "@/public/calendarshadow.png";
+import timershadow from "@/public/timershadow.png";
+import doneshadow from "@/public/doneshadow.png";
+import timer from "@/public/timer.png";
 import Image from "next/image";
 
 const Overview = () => {
@@ -15,6 +20,70 @@ const Overview = () => {
           <Image src={add} alt="add" />
           <h6>Add New Project</h6>
         </button>
+      </div>
+      <div className="grid grid-cols-12 items-center ">
+        <div className="col-span-9 w-full">
+          <div className="flex gap-6">
+            <div className="w-full">
+              <div className="relative border border-black rounded-[24px] overflow-hidden">
+                <div className="">
+                  <div className="bg-[#FC95B4] w-full p-4  rounded-[24px]">
+                    <div className="bg-[#FF7CA4] w-[32px] h-[32px] rounded-[8px] flex justify-center items-center ">
+                      <Image src={calendar} alt="calendar" />
+                    </div>
+                    <h5 className="font-clash_medium text-lg mt-3 mb-2">
+                      Upcoming Tasks
+                    </h5>
+                    <h4 className="font-clash_semibold text-[40px]">40</h4>
+                  </div>
+                </div>
+                <div className="absolute  bottom-0 right-0">
+                  <Image src={calendarshadow} alt="calendarshow" />
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="relative border border-black rounded-[24px] overflow-hidden">
+                <div className="">
+                  <div className="bg-[#FFCE62] w-full p-4  rounded-[24px]">
+                    <div className="bg-[#FFC23D] w-[32px] h-[32px] rounded-[8px] flex justify-center items-center ">
+                      <Image src={timer} alt="timer" />
+                    </div>
+                    <h5 className="font-clash_medium text-lg mt-3 mb-2">
+                      Upcoming Tasks
+                    </h5>
+                    <h4 className="font-clash_semibold text-[40px]">40</h4>
+                  </div>
+                </div>
+                <div className="absolute bottom-0 right-[1px]">
+                  <Image src={timershadow} alt="timershadow" />
+                </div>
+              </div>
+            </div>
+            <div className="w-full ">
+              <div className="relative border border-black rounded-[24px] overflow-hidden">
+                <div className="">
+                  <div className=" bg-[#0BC56D] w-full p-4 rounded-[24px]">
+                    <div className="bg-[#15B569] w-[32px] h-[32px] rounded-[8px] flex justify-center items-center ">
+                      <Image src={calendar} alt="calendar" />
+                    </div>
+                    <h5 className="font-clash_medium text-lg mt-3 mb-2">
+                      Upcoming Tasks
+                    </h5>
+                    <h4 className="font-clash_semibold text-[40px]">40</h4>
+                  </div>
+                </div>
+                <div className="absolute   bottom-0 right-0 ">
+                  <Image
+                    src={doneshadow}
+                    alt="doneshadow"
+                    style={{ zIndex: 0 }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

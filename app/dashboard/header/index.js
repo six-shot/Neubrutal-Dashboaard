a073e5b10e9d152910c6506bc78c4df459fc8043
six-shot <okhuomonajayi54@gmail.com>
2024-04-17 20:39:20 +0000

    settings modal

--- app/dashboard/header/index.js
+++ app/dashboard/header/index.js
@@ -147,37 +147,37 @@ export default function Header() {
         </div>
       </div>
       {/* Mobile header */}
-      <div className="px-[4%] pt-[6%] flex justify-between items-center">
+      <div className="px-[4%] pt-[5%] flex justify-between items-center">
         <div className="flex gap-2 items-center">
-          <div className="w-[50px] h-[50px] rounded-full border border-black flex justify-center items-center">
+          <div className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
             <Image src={user} alt="user" />
           </div>
           <div className="flex flex-col">
-            <h5 className="leading-[18px]">
-              <span className="font-clash_regular text-sm">Welcome</span>{" "}
+            <h5 className="leading-[20px]">
+              <span className="font-clash_regular text-xs">Welcome</span>{" "}
               &#128075;
             </h5>
-            <h5 className=" text-base font-clash_medium">Rohan Kumar</h5>
+            <h5 className=" text-sm font-clash_medium">Rohan Kumar</h5>
           </div>
         </div>
         <div className="flex gap-2">
-          <div className="w-[50px] h-[50px] rounded-full border border-black flex justify-center items-center">
+          <div className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
             <CiSearch
               onClick={() => {
                 setShowModal(true);
               }}
-              className="text-[26px] cursor-pointer"
+              className="text-[24px] cursor-pointer"
             />
           </div>
           <SearchModal onClose={handleClose} visible={showModal} />
 
-          <div className="  w-[50px] h-[50px] rounded-full border border-black flex justify-center items-center">
+          <div className="  w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
             <div className=" relative">
               <IoMdNotificationsOutline
                 onClick={() => {
                   setShowModal(true);
                 }}
-                className=" text-[26px] cursor-pointer"
+                className=" text-[24px] cursor-pointer"
               />
               <h6 className="absolute -top-[2px] right-0 font-clash_medium border border-black text-[10px] w-[15px] h-[15px] flex justify-center items-center rounded-full bg-[#0BC56D]">
                 5

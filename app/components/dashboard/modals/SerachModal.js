706eb1a1788cd3a4788394b4636abb1993ddd278
six-shot <okhuomonajayi54@gmail.com>
2024-04-17 18:51:50 +0000

    settings modal

---
+++ app/components/dashboard/modals/SerachModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { RiSearch2Line } from "react-icons/ri";
+export default function SearchModal({ visible, onClose }) {
+  const handleClose = (e) => {
+    if (e.target.id === "container") onClose();
+  };
+  if (!visible) return null;
+  return (
+    <div
+      id="container"
+      className="fixed z-20 inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-start  pt-[7%] justify-center"
+      onClick={handleClose}
+    >
+      <div className="bg-[#FC95B4] w-[600px] px-[2%] pt-[2%] pb-[7%] rounded-xl">
+        <div className="flex gap-3 items-center">
+          <div className="flex items-center gap-2 px-3 w-full h-[40px] bg-[#FFFAE5] rounded-lg">
+            <RiSearch2Line className="text-[20px] text-[#FC95B4]" />
+            <input
+              type="text"
+              className="w-full h-full px-3 rounded-lg bg-transparent outline-none text-[#FC95B4] placeholder:text-[#FC95B4]"
+              placeholder="Make a search"
+            />
+          </div>
+          <IconContext.Provider value={{ size: "30px" }}>
+            <IoCloseCircleOutline className="text-[#FFFAE5]" />
+          </IconContext.Provider>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/dashboard/header/index.js
+++ app/dashboard/header/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   MdDashboard,
   MdStickyNote2,
@@ -7,10 +8,15 @@ import {
   MdOutlineSettings,
 } from "react-icons/md";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
+import { IoMoonOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
+import { GoClock, GoPlus } from "react-icons/go";
+import { CgLogOut } from "react-icons/cg";
+import { RxUpdate } from "react-icons/rx";
 import HeaderLink from "./headerlinks/headerLinks";
 import user from "@/public/user.png";
 import Image from "next/image";
+import SearchModal from "@/app/components/dashboard/modals/SerachModal";
 
 export default function Header() {
   const headerItems = [
@@ -46,10 +52,13 @@ export default function Header() {
       ],
     },
   ];
+  const [toggle, setToggle] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = () => setShowModal(false);
 
   return (
     <header className="">
-      <div className="w-full flex justify-between items-center h-[11vh] px-[4%] border-b-[1px] border-black">
+      <div className=" relative w-full flex justify-between items-center h-[11vh] px-[4%] border-b-[1px] border-black">
         <div className="">
           <h3 className=" text-xl font-clash_bold">Tasker</h3>
         </div>
@@ -68,12 +77,68 @@ export default function Header() {
           </ul>
         </nav>
         <div className="flex gap-[20px]">
-          <div className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
-            <MdOutlineSettings className="text-[20px]" />
+          <div className="">
+            <div
+              onClick={() => setToggle(!toggle)}
+              className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center"
+            >
+              <MdOutlineSettings className="text-[20px] cursor-pointer" />
+            </div>
+            <div
+              className={`${
+                toggle ? "flex flex-col" : "hidden"
+              } px-3 py-3 bg-[#EB7B26] absolute -bottom-[370px] right-[4%] border border-black  my-2 min-h-[300px] min-w-[300px] rounded-xl sidebar z-10`}
+            >
+              <div className="flex gap-3 items-center ">
+                <div className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
+                  <Image src={user} alt="user" />
+                </div>
+                <div>
+                  <h5 className="text-sm font-clash_medium">Rohan Kumar</h5>
+                  <h6 className="text-[10px] font-clash_regular">
+                    rohankumar77@gmail.com
+                  </h6>
+                </div>
+              </div>
+              <div className="mt-[8%] flex gap-5 flex-col">
+                <div className="flex items-center gap-3">
+                  <IoMoonOutline className="text-[20px]" />
+                  <h5 className="font-clash_regular text-base">Dark Mode</h5>
+                </div>
+                <div className="flex items-center gap-3">
+                  <MdOutlineSettings className="text-[20px]" />
+                  <h5 className="font-clash_regular text-base">Settings</h5>
+                </div>
+                <div className="flex items-center gap-3">
+                  <GoClock className="text-[20px]" />
+                  <h5 className="font-clash_regular text-base">History</h5>
+                </div>
+                <div className="flex items-center gap-3">
+                  <RxUpdate className="text-[20px]" />
+                  <h5 className="font-clash_regular text-base">Updates</h5>
+                </div>
+                <hr className="h-[1px] bg-black border-black" />
+                <div className="flex items-center gap-3">
+                  <GoPlus className="text-[20px]" />
+                  <h5 className="font-clash_regular text-base">Add Account</h5>
+                </div>
+                <div className="flex items-center gap-3">
+                  <CgLogOut className="text-[20px]" />
+                  <h5 className="font-clash_regular text-base">Logout</h5>
+                </div>
+              </div>
+            </div>
           </div>
+
           <div className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
-            <CiSearch className="text-[20px]" />
+            <CiSearch
+              onClick={() => {
+                setShowModal(true);
+              }}
+              className="text-[20px] cursor-pointer"
+            />
           </div>
+          <SearchModal onClose={handleClose} visible={showModal} />
           <div className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
             <Image src={user} alt="user" />
           </div>

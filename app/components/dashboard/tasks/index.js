759f2c7944a314e8b742505d5f08b4ed4d3f6c79
six-shot <okhuomonajayi54@gmail.com>
2024-04-18 18:03:44 +0000

    fix

--- app/components/dashboard/tasks/index.js
+++ app/components/dashboard/tasks/index.js
@@ -6,33 +6,35 @@ import document from "@/public/document.png"
 export default function Task() {
   return (
     <div className="sm:w-[130%] w-[100%]  bg-white border border-black rounded-[24px] p-4">
-      <h5 className="font-clash_medium text-[24px]">Today&apos;s tasks</h5>
-      <div className="flex justify-between items-center py-[21px] border-b-[1px] border-black">
+      <h5 className="font-clash_medium sm:text-[24px] text-[20px]">
+        Today&apos;s tasks
+      </h5>
+      <div className="flex justify-between items-center py-[12px] sm:py-[21px] border-b-[1px] border-black">
         <div className="flex items-start gap-[10px]">
           <div className="sm:w-[53px] w-[43px] h-[43px] sm:h-[53px] border border-black rounded-full flex justify-center items-center">
             <Image src={call} alt="call" />
           </div>
           <div>
-            <h5 className="font-clash_medium text-[19px]">
+            <h5 className="font-clash_medium sm:text-[19px] text-base">
               Call with Backend team
             </h5>
-            <h6 className="text-[#575757] font-clash_medium text-base">
+            <h6 className="text-[#575757] font-clash_medium text-sm sm:text-base">
               1:00 PM - 2:00 PM
             </h6>
           </div>
         </div>
         <div className="sm:w-[21px] w-[18px] h-[18px] sm:h-[21px] rounded-full border border-black"></div>
       </div>
-      <div className="flex justify-between items-center py-[21px] border-b-[1px] border-black">
+      <div className="flex justify-between items-center py-[12px] sm:py-[21px] border-b-[1px] border-black">
         <div className="flex items-start gap-[10px]">
           <div className="sm:w-[53px] w-[43px] h-[43px] sm:h-[53px] border border-[#b3b3b3] rounded-full flex justify-center items-center">
             <Image src={document} alt="call" />
           </div>
           <div>
-            <h5 className="font-clash_medium text-[19px] text-[#b3b3b3] line-through">
+            <h5 className="font-clash_medium sm:text-[19px] text-base text-[#b3b3b3] line-through">
               Create Sprint Backlog
             </h5>
-            <h6 className="text-[#e0e0e0] font-clash_medium text-base">
+            <h6 className="text-[#e0e0e0] font-clash_medium text-sm sm:text-base">
               1:00 PM - 2:00 PM
             </h6>
           </div>
@@ -41,16 +43,16 @@ export default function Task() {
           <Image src={check} alt="check" />
         </div>
       </div>
-      <div className="flex justify-between items-center py-[21px] border-b-[1px] border-black">
+      <div className="flex justify-between items-center py-[12px] sm:py-[21px] ">
         <div className="flex items-start gap-[10px]">
           <div className="sm:w-[53px] w-[43px] h-[43px] sm:h-[53px] border border-black rounded-full flex justify-center items-center">
             <Image src={call} alt="call" />
           </div>
           <div>
-            <h5 className="font-clash_medium text-[19px]">
+            <h5 className="font-clash_medium sm:text-[19px] text-base">
               Video Call with Ram
             </h5>
-            <h6 className="text-[#575757] font-clash_medium text-base">
+            <h6 className="text-[#575757] font-clash_medium text-sm sm:text-base">
               1:00 PM - 2:00 PM
             </h6>
           </div>
